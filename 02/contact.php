a0d540cb04a53f1887bb63ba7800e3715de66cf8
kanako shima <kanakos891@gmail.com>
2016-05-20 11:08:16 +0900

    11:08

--- 02/contact.php
+++ 02/contact.php
@@ -1,43 +1,79 @@
-<form action="result.php" method="get">
-    <label >姓</label>
-    <input  type="textbox" name="sei"></br>
-
-    <label >名</label>
-    <input  type="textbox" name="mei"></br>
-
-    <label >性別</label>
-    <input type="radio" name="radio" value="r1">男
-    <input type="radio" name="radio" value="r2">女
-    <input type="radio" name="radio" value="r3">不明</br>
-
-    <label >アドレス</label>
-    <input type="textbox" name="ad">@<input type="textbox" name="ad"></br>
-
-    <label >電話番号</label>
-    <input type="textbox" name="tel">-<input type="textbox" name="tel">-<input type="textbox" name="tel"></br>
-
-    <label >住所</label>
-    <input type="textbox" name="residence"></br>
-
-    <label >質問のカテゴリ</label>
+<form action="result.php" method="POST">
+<head>
+    <title>お問合せ</title>
+</head>
+<body>
+    <h1>お問合せ</h1>
+    <p>
+    姓
+    <input  type="textbox" name="sei" >
+</p><p>
+    名
+    <input  type="textbox" name="mei" >
+</p><p>
+    性別
+    <input id="rd1" type="radio" name="gender" value="g1">
+    <label for="rd1">男性</label>
+    <input id="rd2" type="radio" name="gender" value="g2">
+    <label for="rd2">女性</label>
+    <input id="rd3" type="radio" name="gender" value="g3">
+    <label for="rd3">不明</label>
+</p><p>
+    住所
+    <input type="textbox" name="residence" size="40">
+</p><p>
+    電話番号
+    <input type="textbox" name="tel1" size="5" >-
+    <input type="textbox" name="tel2" size="5" >-
+    <input type="textbox" name="tel3" size="5" >
+</p><p>
+    アドレス
+    <input type="textbox" name="ad1" >@<input type="textbox" name="ad2" size="10">
+</p><p>
+    どこで知ったか
+    <input id="ch1" type="checkbox" name="know" value="know1">
+    <label for="ch1">雑誌</label>
+    <input id="ch2" type="checkbox" name="know" value="know2">
+    <label for="ch2">新聞</label>
+    <input id="ch3" type="checkbox" name="know" value="know3">
+    <label for="ch3">テレビ</label>
+    <input id="ch4" type="checkbox" name="know" value="know4">
+    <label for="ch4">その他</label>
+</p><p>
+    質問のカテゴリ
     <select name="category">
-        <option>あ</option>
-        <option>い</option>
-        <option>う</option>
-        <option>え</option>
-    </select></br>
-
-    <label >質問内容</label>
-    <textarea clos="30" rows="5" name="coments">
-        内容をどうぞ。
-    </textarea></br>
-
-    <label >どこで知ったか</label>
-    <input type="checkbox" name="know" value="know1">
-    <input type="checkbox" name="know" value="know2">
-    <input type="checkbox" name="know" value="know3">
-    <input type="checkbox" name="know" value="know4"></br>
-
+        <?php
+        $category=array('あ','い','う','え','お');
+        foreach($category as $cat){
+            echo "<option value=\"{$cat}\"> {$cat}</option>";
+        }
+        ?>
+        </select>
+</p><p>
+    質問内容<br>
+    <textarea  name="coments" >
+    記載してください。
+    </textarea>
+</p>
     <input type="submit">
-
+</body>
 </form>
+<style>
+{
+    text-align: center;
+}
+form{
+    border-radius: 5px;
+    text-align: center;
+    padding: 2em 5em;
+    background: #F0F8FF;}
+p{
+    border-radius: 5px;
+    font-weight: bold;
+
+}
+label{
+}
+textarea{
+
+}
--- 02/result.php
+++ 02/result.php
@@ -1,11 +1,88 @@
 <?php
-echo "姓名".$_GET['sei'].$_GET['mei']."<br>";
-echo "性別".$_GET['radio']."<br>";
-echo "アドレス".$_GET['ad']."<br>";
-echo "電話番号".$_GET['tel']."<br>";
-echo "住所".$_GET['residence']."<br>";
-echo "質問のカテゴリ".$_GET['category']."<br>";
-echo "質問内容".$_GET['coments']."<br>";
 
+echo "お問合せ完了<br>";
+echo "姓名";
+if(is_numeric($_POST["sei"]) == false && is_numeric($_POST["mei"]) == false){
+    echo "入力しなおしてください.<br>";
+}else{
+    echo $_POST['sei']." ".$_POST['mei']."<br>";
+}
 
-?>
+//性別表示
+echo "性別";
+if(isset($_POST["gender"]) == false){
+    echo "チェックしなおしてください<br>";
+}
+elseif($_POST["gender"] === "g1"){
+    echo "男性<br>";
+}
+elseif($_POST["gender"] === "g2"){
+    echo "女性<br>";
+}
+else{
+    echo "不明<br>";
+}
+
+echo "住所";
+if(is_numeric($_POST["residence"]) == false){
+    echo "入力しなおしてください.<br>";
+}else{
+    echo $_POST['residence']."<br>";
+}
+
+echo "電話番号";
+if(is_numeric($_POST["tel1"]) == false && is_numeric($_POST["tel2"]) == false){
+    if(is_numeric($_POST["tel2"]) == false && is_numeric($_POST["tel3"]) == false){
+        echo "入力しなおしてください.<br>";
+    }
+}
+else{
+    echo $_POST['tel1']."-".$_POST['tel2']."-".$_POST['tel3']."<br>";
+}
+
+echo "アドレス";
+if(is_numeric($_POST["ad1"]) == false && is_numeric($_POST["ad2"]) == false){
+    echo "入力しなおしてください.<br>";
+}
+else{
+    echo $_POST['ad1']."@".$_POST['ad2']."<br>";
+}
+//どこで知ったか
+echo "どこで知ったか";
+if(isset($_POST["know"]) == false){
+    echo "チェックしなおしてください<br>";
+}
+//$know =$_POST["know"];
+elseif(isset($_POST["know"][0])){
+    echo "雑誌<br>";
+}
+elseif(isset($_POST["know"][1])){
+    echo "新聞<br>";
+}
+elseif(isset($_POST["know"][2])){
+    echo "テレビ<br>";
+}
+elseif(isset($_POST["know"][3])){
+    echo "その他<br>";
+}
+
+//質問のカテゴリ
+echo "質問のカテゴリ";
+//$category =$_POST["category"];
+if(isset($_POST["category"][0])){
+    echo "あ <br>";
+}
+elseif(isset($_POST["category"][1])){
+    echo "い <br>";
+}
+elseif(isset($_POST["category"][2])){
+    echo "う <br>";
+}
+elseif(isset($_POST["category"][3])){
+    echo "え <br>";
+}
+elseif(isset($_POST["category"][4])){
+    echo "お <br>";
+}
+
+echo "質問内容<br>".$_POST['coments']."<br>";
