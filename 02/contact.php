6f87408e9107e2ac22055a24ec78f79e98d79eb2
kanako shima <kanakos891@gmail.com>
2016-05-20 13:49:45 +0900

    13:49

--- 02/contact.php
+++ 02/contact.php
@@ -1,36 +1,48 @@
-<form action="result.php" method="POST">
+
 <head>
     <title>お問合せ</title>
 </head>
+<form action="result.php" method="pOST">
+<h1>お問合せ</h1>
 <body>
-    <h1>お問合せ</h1>
-    <p>
-    姓
-    <input  type="textbox" name="sei" >
-</p><p>
-    名
+    <div class="mod_dl_table">
+        <dl>
+        <dt>姓</dt>
+            <dd>
+        <input  type="text" name="sei" size="15" maxlength="40" class="input-text" >
+    </dd>
+        <dt>名</dt>
+        <dd>
     <input  type="textbox" name="mei" >
-</p><p>
-    性別
-    <input id="rd1" type="radio" name="gender" value="g1">
+</dd>
+
+    <dt>性別</dt>
+<dd><input id="rd1" type="radio" name="gender" value="g1">
     <label for="rd1">男性</label>
     <input id="rd2" type="radio" name="gender" value="g2">
     <label for="rd2">女性</label>
     <input id="rd3" type="radio" name="gender" value="g3">
     <label for="rd3">不明</label>
-</p><p>
-    住所
-    <input type="textbox" name="residence" size="40">
-</p><p>
-    電話番号
+</dd>
+
+    <dt>住所</dt>
+    <dd><input type="textbox" name="residence" size="40">
+    </dd>
+
+    <dt>電話番号</dt>
+    <dd>
     <input type="textbox" name="tel1" size="5" >-
     <input type="textbox" name="tel2" size="5" >-
     <input type="textbox" name="tel3" size="5" >
-</p><p>
-    アドレス
+</dd>
+
+    <dt>アドレス</dt>
+    <dd>
     <input type="textbox" name="ad1" >@<input type="textbox" name="ad2" size="10">
-</p><p>
-    どこで知ったか
+</dd>
+
+    <dt>どこで知ったか</dt>
+    <dd>
     <input id="ch1" type="checkbox" name="know" value="know1">
     <label for="ch1">雑誌</label>
     <input id="ch2" type="checkbox" name="know" value="know2">
@@ -39,8 +51,10 @@
     <label for="ch3">テレビ</label>
     <input id="ch4" type="checkbox" name="know" value="know4">
     <label for="ch4">その他</label>
-</p><p>
-    質問のカテゴリ
+</dd>
+
+    <dt>質問のカテゴリ</dt>
+    <dd>
     <select name="category">
         <?php
         $category=array('あ','い','う','え','お');
@@ -49,31 +63,41 @@
         }
         ?>
         </select>
-</p><p>
-    質問内容<br>
+    </dd>
+
+    <dt>質問内容<br></dt>
+
     <textarea  name="coments" >
     記載してください。
     </textarea>
-</p>
+
+
+
     <input type="submit">
+</div>
 </body>
 </form>
-<style>
-{
-    text-align: center;
-}
+<style type="text/css">
+
 form{
-    border-radius: 5px;
-    text-align: center;
+    margin: auto;
+    border: 3px;
     padding: 2em 5em;
     background: #F0F8FF;}
-p{
-    border-radius: 5px;
-    font-weight: bold;
-
-}
-label{
+dt{
+    float: center;
+    width: 150px;
+    font-weight: normal;
+    padding: 5px;
+    border-top: 1px ;
+    margin-top: -1px;}
+dd{
+    margin-left: 150px;
+    width: 100%;
+    font-weight: normal;
+    border-top: 1px ;
+    margin-top: -1px;
 }
 textarea{
-
+    margin-left: 150px;
 }
